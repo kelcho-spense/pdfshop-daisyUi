f7517956bba2dc39392b30aa64d5dee9f851a13b
kelcho-spense <kelchospense88@gmail.com>
2024-06-18 16:33:52 +0300

    router done

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider, } from "react-router-dom";
 import './index.css'
-import HomePage from './Pages/HomePage.tsx'
-import ErrorPage from './Pages/ErrorPage.tsx';
+import HomePage from './Pages/Home.tsx'
+import ErrorPage from './Pages/Error.tsx';
 import Contact from './Pages/Contact.tsx';
+import { Login } from './Pages/Login.tsx';
+import { Register } from './Pages/Register.tsx';
+import { DashBoard } from './Pages/DashBoard.tsx';
 
 const router = createBrowserRouter([
   {
@@ -13,9 +16,24 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
   },
   {
-    path: "contacts/:contactId",
+    path: "auth/login",
+    element: <Login />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "register",
+    element: <Register />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "contact/",
     element: <Contact />,
   },
+  {
+    path: "dashboard",
+    element: <DashBoard />,
+    errorElement: <ErrorPage />
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
