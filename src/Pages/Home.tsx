4014461c1d4936b35d0808062efadf35de05d33c
kelcho-spense <kelchospense88@gmail.com>
2024-06-20 01:00:32 +0300

    Home page 80% done

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,9 +1,22 @@
 import { ScrollRestoration } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Companies from "../components/Companies";
+import JoinUs from "../components/JoinUs";
+import Benefits from "../components/Benefits";
+import { benefitOne, benefitTwo } from '../utils/data'
+import { Container } from "../components/Container";
 const HomePage = () => {
   return (
-    <div>HomePage
+    <Container className="bg-base-200 flex flex-col gap-6">
+      <Navbar />
+      <Hero />
+      <Companies />
+      <JoinUs />
+      <Benefits data={benefitOne} />
+      <Benefits imgPos="right" data={benefitTwo} />
       <ScrollRestoration />
-    </div>
+    </Container>
   )
 }
 
